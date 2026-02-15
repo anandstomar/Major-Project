514bbb78a480850589185da7d824ab1f6dcfe9f8
anandstomar <anandsinghtomar25.com>
2026-02-15 18:45:44 +0530

    dsewytdewyduyeew

--- services/auth/src/auth/jwt.strategy.ts
+++ services/auth/src/auth/jwt.strategy.ts
@@ -10,10 +10,10 @@ export class JwtAuthStrategy extends PassportStrategy(JwtStrategy, 'jwt') {
   private readonly logger = new Logger(JwtAuthStrategy.name);
 
   constructor(private readonly config: ConfigService) {
-    const keycloakUrl = config.get<string>('KEYCLOAK_URL') || 'http://keycloak.default.svc.cluster.local';
+    const keycloakUrl = config.get<string>('KEYCLOAK_URL') || 'http://92.4.78.222/auth-server/realms/provenance';
     const realm = config.get<string>('KEYCLOAK_REALM') || 'provenance';
     const issuer = config.get<string>('KEYCLOAK_ISSUER') || `${keycloakUrl}/realms/${realm}`;
-    const jwksUri = config.get<string>('KEYCLOAK_JWKS') || `http://keycloak.default.svc.cluster.local/realms/provenance/protocol/openid-connect/certs`;
+    const jwksUri = config.get<string>('KEYCLOAK_JWKS') || 'http://92.4.78.222/auth-server/realms/provenance/protocol/openid-connect/certs';
     // audience: optional. If you see audience problems, leave undefined or set to the token's aud/azp
     // const audience = config.get<string>('KEYCLOAK_AUDIENCE') || undefined;
 
