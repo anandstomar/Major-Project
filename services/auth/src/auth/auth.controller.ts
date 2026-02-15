1a4e53eead77487f3b8dc92ec0a4a8cdaf3470e3
anandstomar <anandsinghtomar25.com>
2026-02-15 20:11:07 +0530

    hdjgdguihchjhjshj

--- services/auth/src/auth/auth.controller.ts
+++ services/auth/src/auth/auth.controller.ts
@@ -1,20 +1,20 @@
 // src/auth/auth.controller.ts
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { Request as ExpressRequest } from 'express';
-import { AuthGuard } from '@nestjs/passport';
+//import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from './roles.guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { Roles } from './roles.decorator';
 
 @Controller('auth')               // combined with global prefix 'api/v1' => /api/v1/auth
 export class AuthController {
-  @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Get('me')
   me(@Req() req: ExpressRequest) {
     return { user: req.user };
   }
 
-  @UseGuards(AuthGuard('jwt'), RolesGuard, JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('manage-account')   // choose a role that exists in token
   @Get('admin')
   admin(@Req() req: ExpressRequest) {
