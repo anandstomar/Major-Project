812b0058477a34f158e63c326c2cee00c165efc4
anandstomar <anandsinghtomar25.com>
2026-02-15 18:23:41 +0530

    dsbdsjffcvjhbhdjbd

--- services/auth/src/auth/oidc.config.ts
+++ services/auth/src/auth/oidc.config.ts
@@ -1,6 +1,6 @@
 export const oidcConfig = {
-  issuer: process.env.KEYCLOAK_ISSUER || 'http://localhost:8093/realms/provenance',
+  issuer: process.env.KEYCLOAK_ISSUER || '"http://92.4.78.222/auth-server/realms/provenance"',
   audience: process.env.KEYCLOAK_AUDIENCE || 'provenance-api',
   // you can override JWKS URI explicitly if needed:
-  jwksUri: process.env.KEYCLOAK_JWKS_URI || `${process.env.KEYCLOAK_ISSUER || 'http://keycloak.default.svc.cluster.local/realms/provenance'}/protocol/openid-connect/certs`
+  jwksUri: process.env.KEYCLOAK_JWKS_URI || `${process.env.KEYCLOAK_ISSUER || '"http://92.4.78.222/auth-server/realms/provenance"'}/protocol/openid-connect/certs`
 };
