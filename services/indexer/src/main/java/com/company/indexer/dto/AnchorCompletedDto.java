ef3af9695b16bbb9b433618fb94e22019ad6cb91
anandstomar <anandsinghtomar25.com>
2026-02-19 22:30:55 +0530

    sbhcdkjhzJBjkckd

--- services/indexer/src/main/java/com/company/indexer/dto/AnchorCompletedDto.java
+++ services/indexer/src/main/java/com/company/indexer/dto/AnchorCompletedDto.java
@@ -1,16 +1,37 @@
 package com.company.indexer.dto;
 
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AnchorCompletedDto {
+
+    @JsonProperty("request_id")
     public String request_id;
+
+    @JsonProperty("merkle_root")
     public String merkle_root;
+
+    @JsonProperty("tx_hash")
     public String tx_hash;
+
+    @JsonProperty("block_number")   
     public Long block_number;
+
+    @JsonProperty("submitted_at")
     public String submitted_at;
+
+    @JsonProperty("submitter")
     public String submitter;
+
+    @JsonProperty("status")
     public String status;
+
+    @JsonProperty("preview_ids")
     public List<String> preview_ids;
+
+    @JsonProperty("events")
     public List<String> events;
 
     // default constructor, getters/setters optional since Jackson can set public fields
